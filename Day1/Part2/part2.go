365570273f992a8d612ad9bcfac22d0c6da51e8f
Colin Beatty <colin.beatty@tcgplayer.com>
2021-12-01 13:32:22 -0500

    completed part 2

--- Day1/Part2/part2.go
+++ Day1/Part2/part2.go
@@ -1,1 +1,22 @@
 package part2
+
+import (
+	shr "day1/shared"
+)
+
+func Solve() int {
+	stLines := shr.ReadFileLines("part1/input.txt")
+	intLines := shr.ConvertStringArrToIntArr(stLines)
+	count := 0
+
+	for ind := 1; ind < len(intLines)-2; ind++ {
+		current := intLines[ind-1] + intLines[ind] + intLines[ind+1]
+		next := intLines[ind] + intLines[ind+1] + intLines[ind+2]
+
+		if next > current {
+			count++
+		}
+	}
+
+	return count
+}
--- Day1/main.go
+++ Day1/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	p1 "day1/part1"
+	p2 "day1/part2"
 	"fmt"
 )
 
 func main() {
 	fmt.Println(p1.Solve())
+	fmt.Println(p2.Solve())
 }
